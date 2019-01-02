8b9ef6a7de51103810492a0d2734b39ead3d6f71
stf-hdr <stf@hdr.com>
2019-01-02 18:16:36 +0100

    adding initial code with just comments

---
+++ src/ardu-switch-arlo/ardu-switch-arlo.ino
@@ -0,0 +1,14 @@
+void setup() {
+  // put your setup code here, to run once:
+
+  /* init RFID tag */
+  /* init wireless transmitter */
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+  /* read RFID tag */
+  /* transmit signal to receiver */
+  /* enter power saving mode */
+}
